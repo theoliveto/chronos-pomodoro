3813ff9ba177fd6369272b902e35e2def65e1b09
theoliveto <olivetoovinicius@gmail.com>
2026-06-23 11:46:39 -0300

    Criação e estilização do componente Logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+
 import './styles/theme.css'
 import './styles/global.css'
-
-import { Container } from './components/Container'
-import { Heading } from './components/Heading';
 
 export function App() {
     return (
         <>
             <Container>
-                <Heading>LOGO</Heading>
+                <Logo />
             </Container>
         </>
     );

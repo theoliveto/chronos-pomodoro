42b8f5b972387752f9a1607287ba9d3e29205d20
theoliveto <olivetoovinicius@gmail.com>
2026-06-23 09:43:27 -0300

    Ajustes nos arquivos config e mudanças visuais do componente Heading.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading.tsx';
 
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
     return (
         <>
-            <Heading>Olá Mundo</Heading>
+            <Heading>
+                Olá Mundo
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since 1966, 
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,11 @@
 import styles from './Heading.module.css'
 
-export function Heading(props) {
+type HeadingProps = {
+    children: React.ReactNode
+}
+
+export function Heading({ children }: HeadingProps) {
     return (
-        <h1 className={styles.heading}>{props.children}</h1>
+        <h1 className={styles.heading}>{children}</h1>
     );
 }

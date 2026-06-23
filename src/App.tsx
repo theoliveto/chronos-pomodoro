c2fa7e3771c31a16632580464aaa1bcc430ac6b6
theoliveto <olivetoovinicius@gmail.com>
2026-06-23 10:56:24 -0300

    Reorganização de pastas dos componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { Heading } from './components/Heading.tsx'; 
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container'
+import { Heading } from './components/Heading';
 
 export function App() {
     return (
         <>
-            <Heading>
-                Olá Mundo
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since 1966, 
-                when designers at Letraset and James Mosley, 
-                the librarian at St Bride Printing Library in London, 
-                took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.
-            </p>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
         </>
     );
 }
